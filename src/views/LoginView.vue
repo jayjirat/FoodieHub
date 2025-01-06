e6799405ceefa2c13a4dc40cfd68a5e629fb00d6
Jirat Charoenkaew <6510615054@student.tu.ac.th>
2025-01-07 00:25:56 +0700

    Login view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div class="hero bg-base-200 h-screen">
+    <div class="hero-content flex-col">
+      <div class="text-center mb-4">
+        <h1 class="text-5xl font-bold">E commerce project</h1>
+      </div>
+      <div class="card bg-base-100 w-full shrink-0 shadow-2xl">
+        <form class="card-body">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="username"
+              class="input input-bordered"
+              required
+            />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              class="input input-bordered"
+              required
+            />
+          </div>
+          <div class="form-control mt-6">
+            <RouterLink :to="{
+                name:'home-view'
+            }" class="btn btn-primary">Login</RouterLink>
+          </div>
+          <a class="label-text-alt link link-hover" href="#"
+            >Login via google email</a
+          >
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
