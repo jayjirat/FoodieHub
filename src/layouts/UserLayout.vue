092d4b734d877e9d906d1a7affd5af2f07189497
Jirat Charoenkaew <6510615054@student.tu.ac.th>
2025-01-09 03:52:43 +0700

    Footer v1

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -5,12 +5,16 @@ import { RouterLink, useRouter } from "vue-router";
 import { useCartStore } from "@/stores/user/cartStore";
 import { useRestaurantStore } from "@/stores/user/restaurantStore";
 import { useAccountStore } from "@/stores/account";
-
+import { getDownloadURL, ref as sRef } from "firebase/storage";
+import { storage } from "@/firebase";
 const router = useRouter();
 
 const restaurantStore = useRestaurantStore();
 const accountStore = useAccountStore();
 const cartStore = useCartStore();
+
+let github = "";
+let ig = "";
 
 const isDark = ref(false);
 const saveTheme = localStorage.getItem("theme");
@@ -42,6 +46,21 @@ const toggleTheme = () => {
   document.documentElement.setAttribute("data-theme", theme);
   localStorage.setItem("theme", theme);
 };
+
+const getImageUrl = async (fileName) => {
+  try {
+    const storageRef = sRef(storage, `logo/${fileName}`);
+    const url = await getDownloadURL(storageRef);
+    return url;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+onMounted(async () => {
+  github = await getImageUrl("GitHub_Logo_White.png");
+  console.log(github);
+});
 </script>
 
 <template>
@@ -265,27 +284,15 @@ const toggleTheme = () => {
         ></path>
       </svg>
       <p>
-        ACME Industries Ltd.
+        Foodie Hub - Order your favorite food anytime!
         <br />
-        Providing reliable tech since 1992
+        © 2025 Foodie Hub. All rights reserved.
       </p>
     </aside>
     <nav>
       <h6 class="footer-title">Social</h6>
       <div class="grid grid-flow-col gap-4">
-        <a>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            class="fill-current"
-          >
-            <path
-              d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
-            ></path>
-          </svg>
-        </a>
+        <a> <img :src="github" alt="" class="h-6" /> </a>
         <a>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -296,19 +303,6 @@ const toggleTheme = () => {
           >
             <path
               d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
-            ></path>
-          </svg>
-        </a>
-        <a>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            class="fill-current"
-          >
-            <path
-              d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
             ></path>
           </svg>
         </a>
--- src/views/SuccessView.vue
+++ src/views/SuccessView.vue
@@ -21,13 +21,11 @@ onMounted(() => {
 <template>
   <UserLayout>
     <div
-      class="flex w-10/12 h-96 shadow-2xl mx-auto my-4 justify-center items-center flex-col p-8 bg-white rounded-lg"
+      class="flex w-10/12 h-96 shadow-2xl mx-auto my-4 justify-center items-center flex-col p-8 rounded-lg"
     >
       <div class="text-4xl font-bold mb-4">Thank You!</div>
-      <p class="text-lg text-gray-600 mb-2">
-        Your order has been successfully placed.
-      </p>
-      <p class="text-md text-gray-500 mb-4">
+      <p class="text-lg mb-2">Your order has been successfully placed.</p>
+      <p class="text-md mb-4">
         We appreciate your purchase and hope you enjoy your meal.
       </p>
       <span class="loading loading-dots loading-lg"></span>
